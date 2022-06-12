5ed2ded8f7708bad731376e32a7aee85185e30e2
UBiiiii <willowman55@gmail.com>
2022-06-12 16:16:32 +0900

    delete unusable files

--- UI_GPIO/test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-
-monitor_height = root.winfo_screenheight()
-monitor_width = root.winfo_screenwidth()
-
-print("{} x {}".format(monitor_width, monitor_height))
